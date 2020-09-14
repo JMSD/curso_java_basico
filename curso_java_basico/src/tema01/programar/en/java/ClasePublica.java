764c802a6ee5dafa08e11ca9ea494cf1885d01fc
JM <JM@DESKTOP-DL6CQTO>
2020-09-14 16:24:32 +0200

    Subida 01: clases correspondientes al Tema 01

---
+++ curso_java_basico/src/tema01/programar/en/java/ClasePublica.java
@@ -0,0 +1,15 @@
+package tema01.programar.en.java;
+
+/**
+* Segundo ejemplo del tema 01. Escribimos una línea de forma directa
+*
+* @author JM Serrano
+* @version 1.0, 01/09/2020
+*/
+
+public class ClasePublica {
+	public static void main(String[] args) {
+		System.out.println("Estoy en una clase publica");
+	}
+
+}
---
+++ curso_java_basico/src/tema01/programar/en/java/PrimerSaludo.java
@@ -0,0 +1,24 @@
+package tema01.programar.en.java;
+
+/**
+* Primer ejemplo del tema 01. Escribimos tres líneas, una de las cuales se 
+* imprime a través del método mostrarMensaje()
+*
+* @author JM Serrano
+* @version 1.0, 01/09/2020
+*/
+
+public class PrimerSaludo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Jose Maria");
+		PrimerSaludo ps = new PrimerSaludo();
+		ps.mostrarMensaje();
+		System.out.println("FIN DEL PROGRAMA");
+	}
+
+	void mostrarMensaje() {
+		System.out.println("Primera clase del curso de Java");
+	}
+
+}
