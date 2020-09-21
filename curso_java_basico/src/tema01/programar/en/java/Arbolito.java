05e873a2bebf42acda57f62e0853804bf7e48c88
JM <JM@DESKTOP-DL6CQTO>
2020-09-21 13:04:22 +0200

    Subida con todos los ejemplos del tema 01

---
+++ curso_java_basico/src/tema01/programar/en/java/Arbolito.java
@@ -0,0 +1,27 @@
+package tema01.programar.en.java;
+
+/**
+* Ejemplo 04. Dibujamos un pequeño árbol
+* 
+* @author JM Serrano
+* @version 1.0, 21/09/2020
+*/
+
+public class Arbolito {
+
+	public static void main(String[] args) {
+		System.out.println("\n");
+		
+		System.out.println("       *       ");
+		System.out.println("      * *      ");
+		System.out.println("     * * *     ");
+		System.out.println("    * * * *    ");
+		System.out.println("   * * * * *   ");
+		System.out.println("  * * * * * *  ");
+		System.out.println(" * * * * * * * ");
+		System.out.println("      * *      ");
+		
+		System.out.println("\n" + "¡Feliz Navidad!");
+	}
+
+}
---
+++ curso_java_basico/src/tema01/programar/en/java/HolaMundo.java
@@ -0,0 +1,17 @@
+package tema01.programar.en.java;
+
+/**
+* Ejemplo 03. Como es tradicional, el primer programa escrito en cualquier 
+* lenguaje que aprendamos, debe ser el "Hola, Mundo"
+* 
+* @author JM Serrano
+* @version 1.0, 21/09/2020
+*/
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+			System.out.println("Hola Mundo!!!");
+	}
+
+}
